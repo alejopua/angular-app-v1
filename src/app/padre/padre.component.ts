7baac90b93d0c28c3537b009c1f5cd50d7880adb
alejopua <alejopua@gmail.com>
2024-04-09 23:44:57 -0500

    Refactor PadreComponent to implement OnInit and add initialization logic

--- src/app/padre/padre.component.ts
+++ src/app/padre/padre.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ServerFamilyService } from '../server-family.service';
 
 @Component({
@@ -6,11 +6,25 @@ import { ServerFamilyService } from '../server-family.service';
   templateUrl: './padre.component.html',
   styleUrls: ['./padre.component.css'],
 })
-export class PadreComponent {
+export class PadreComponent implements OnInit {
   msjFather: string = 'Mensaje desde el padre';
   receiveMsj?: string;
+  name?: string;
 
   constructor(private _serverFamily: ServerFamilyService) {}
+
+  ngOnInit(): void {
+    this._serverFamily.setFather('Father');
+    this.name = this._serverFamily.getFather();
+  }
+
+  greeting(): void {
+    this._serverFamily.greeting(this._serverFamily.getBigBrother() || '');
+  }
+
+  ask() {
+    console.log(this._serverFamily.ask());
+  }
 
   receiveMsjFromSon($event: string) {
     this.receiveMsj = $event;
